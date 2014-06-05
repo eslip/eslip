c7de8a9a3441128a12ad4bdf9af32f7632426bc4
Nicolas Burghi <nicoburghi@gmail.com>
2014-06-05 20:05:44 -0300

    actualizacion de estilos del ejemplo

--- example/index.php
+++ example/index.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
+	<meta charset="utf-8">
+	<title>ESLIP Example</title>
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="example.css" />
     <link rel="stylesheet" type="text/css" href="../eslip/frontend/eslip_plugin.css" />
     <script type="text/javascript" src="../eslip/frontend/eslip_plugin.js"></script>
 </head>
-<body>
-<div id="ESLIP_Plugin"></div>
+<body class="example-widget">
+	<div class="container">
+		<div class="header">
+			<a href="http://eslip.com.ar" title="Sitio Web Oficial de ESLIP" target="_blank">
+				<img id="eslip-logo" src="../eslip/backend/images/eslip-logo.png" alt="ESLIP" >
+			</a>
+		</div>
+		<div class="content example-content">
+			<div id="ESLIP_Plugin"></div>
+		</div>
+		<div class="footer">
+			<span class="pull-left">[nicoburghi@gmail.com] Nicolás Burghi</span>
+			<span class="pull-right">Martín Estigarribia [martinestiga@gmail.com]</span>
+		</div>
+	</div>
 </body>
 </html>
--- example/login.php
+++ example/login.php
@@ -1,17 +1,57 @@
-<?php
-
-/*
-**	Acá el desarrollador va hacer el login correspondiente en su sitio con los datos que le enviamos por POST o tambien lo puede obtener de la SESSION
-*/
-
-session_start();
-
-var_dump($_POST);
-
-if($_POST['state'] == 'success'){
-	var_dump(json_decode($_POST['user']));	
-}else{
-	var_dump(json_decode($_POST['error']));	
-}
-
-var_dump($_SESSION);
+<?if(!isset($_POST['state'])) header('Location: index.php'); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>ESLIP Example</title>
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="example.css" />
+</head>
+<body class="results">
+	<div class="container">
+		<div class="header">
+			<a href="http://eslip.com.ar" title="Sitio Web Oficial de ESLIP" target="_blank">
+				<img id="eslip-logo" src="../eslip/backend/images/eslip-logo.png" alt="ESLIP" >
+			</a>
+		</div>
+		<div class="content">
+			<?php 
+			/*
+			* Acá el desarrollador va hacer el login correspondiente en su sitio 
+			* con los datos que le enviamos por POST o tambien lo puede obtener de la SESSION
+			*/
+			?>
+			<?php session_start(); ?>
+			<h2>Datos generales:</h2>
+			<div class="row">
+				<label>Proveedor de identidad: </label>
+				<span><?php echo $_POST['server'] ?></span>
+			</div>
+			<div class="row">
+				<label>Origen:</label>
+				<span><?php echo $_POST['referer'] ?></span>
+			</div>
+			<div class="row">
+				<label>Estado:</label>
+				<span class="<?php echo $_POST['state'] ?>"><?php echo $_POST['state'] ?></span>
+			</div>
+			<?php if($_POST['state'] == 'success'){ ?>
+				<h2 class="hola">Hola <?php echo $_POST['user_identification'];?>!! Gracias por probar ESLIP!</h2> 
+				<h2>Recursos obtenidos:</h2>
+				<pre>
+					<?php print_r(json_decode($_POST['user'])); ?>
+				</pre>
+			<?php }else{ ?>
+				<h2>Datos del error</h2>
+				<pre>
+					<?php print_r(json_decode($_POST['error'])); ?>
+				</pre>
+			<?php } ?>
+		</div>
+		<div class="footer">
+			<span class="pull-left">[nicoburghi@gmail.com] Nicolás Burghi</span>
+			<span class="pull-right">Martín Estigarribia [martinestiga@gmail.com]</span>
+		</div>
+	</div>
+</body>
+</html>
